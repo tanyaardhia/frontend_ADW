8c3581efed0c2d2252f408ebd52ddb1a6d0c80ad
TanyaArdhia <tanyaardhiap@gmail.com>
2024-08-07 21:45:37 +0700

    fic: navbar

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,7 +1,77 @@
+import Link from "next/link";
+
 export default function Login() {
   return (
-    <div>
-      <p>Loginnnnnnnnnn nih</p>
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="bg-gradient-to-r from-[#5B99C2] to-[#1A4870] rounded-[20px] p-4">
+        <div className="border-[10px] border-transparent rounded-[20px] dark:bg-gray-900 bg-white shadow-lg p-4 sm:p-6 md:p-8 lg:p-10">
+          <h1 className="pt-4 pb-4 font-bold dark:text-gray-400 text-3xl text-center cursor-default">
+            Log in
+          </h1>
+          <form action="#" method="post" className="space-y-3">
+            <div>
+              <label
+                htmlFor="email"
+                className="mb-1 dark:text-gray-400 text-sm block"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                className="border p-2 dark:bg-indigo-700 dark:text-gray-300 dark:border-gray-700 shadow-md placeholder:text-sm focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
+                type="email"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="mb-1 dark:text-gray-400 text-sm block"
+              >
+                Password
+              </label>
+              <input
+                id="password"
+                className="border p-2 shadow-md dark:bg-indigo-700 dark:text-gray-300 dark:border-gray-700 placeholder:text-sm focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div>
+              <button
+                className="bg-gradient-to-r dark:text-gray-300 from-[#5B99C2] to-[#1A4870] shadow-lg p-2 text-white rounded-lg w-full hover:scale-105 hover:from-[#1A4870] hover:to-[#5B99C2] transition duration-300 ease-in-out"
+                type="submit"
+              >
+                LOG IN
+              </button>
+            </div>
+          </form>
+          <div className="text-gray-500 flex text-center flex-col mt-3 items-center text-xs">
+            <p className="cursor-default">
+              By signing in, you agree to our
+              <a
+                className="group text-blue-400 transition-all duration-100 ease-in-out ml-1"
+                href="#"
+              >
+                <span className="cursor-pointer bg-left-bottom bg-gradient-to-r from-blue-400 to-blue-400 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                  Terms
+                </span>
+              </a>{" "}
+              and
+              <a
+                className="group text-blue-400 transition-all duration-100 ease-in-out ml-1"
+                href="#"
+              >
+                <span className="cursor-pointer bg-left-bottom bg-gradient-to-r from-blue-400 to-blue-400 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                  Privacy Policy
+                </span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -11,86 +11,80 @@ export default function Navbar() {
 
   return (
     <>
-      <div className="top-0 py-1 lg:py-2 w-full bg-transparent lg:relative z-50 dark:bg-gray-900 bg-[#E2DAD6]">
-        <nav className="z-10 sticky top-0 left-0 right-0 max-w-4xl xl:max-w-5xl mx-auto px-5 py-2.5 lg:border-none lg:py-4">
-          <div className="flex items-center justify-between">
-            <button>
-              <div className="flex items-center space-x-2">
-                <h2 className="text-black uppercase dark:text-white font-bold text-2xl">
-                  <Link href="/">Listify</Link>
-                </h2>
+      <div>
+        <div className="top-0 py-1 lg:py-2 w-full lg:relative z-50 dark:bg-gray-900 bg-[#E2DAD6]">
+          <nav className="z-10 sticky top-0 left-0 right-0 max-w-4xl xl:max-w-5xl mx-auto px-5 py-2.5 lg:border-none lg:py-4">
+            <div className="flex items-center justify-between">
+              <button>
+                <div className="flex items-center space-x-2">
+                  <h2 className="text-black uppercase dark:text-white font-bold text-2xl">
+                    <Link href="/">Listify</Link>
+                  </h2>
+                </div>
+              </button>
+
+              <div className="hidden lg:block">
+                <ul className="flex space-x-10 text-base font-bold text-black/60 dark:text-white">
+                  <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
+                    <Link href="/">Home</Link>
+                  </li>
+                  <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
+                    <Link href="/dashboard">Dashboard</Link>
+                  </li>
+                </ul>
               </div>
-            </button>
 
-            <div className="hidden lg:block">
-              <ul className="flex space-x-10 text-base font-bold text-black/60 dark:text-white">
-                <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
-                  <Link href="/">Home</Link>
-                </li>
-                <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
-                  <Link href="/dashboard">Dashboard</Link>
-                </li>
-              </ul>
-            </div>
+              <div className="hidden lg:flex lg:items-center gap-x-2">
+                <button className="flex items-center justify-center rounded-md bg-[#5B99C2] text-white px-6 py-2.5 font-semibold hover:shadow-lg hover:drop-shadow transition duration-200">
+                  <Link href="/login">Login</Link>
+                </button>
+              </div>
 
-            <div className="hidden lg:flex lg:items-center gap-x-2">
-              <button className="flex items-center text-black dark:text-white justify-center px-6 py-2.5 font-semibold hover:text-[#1A4870]">
-                <Link href="/register">Sign up</Link>
-              </button>
-              <button className="flex items-center justify-center rounded-md bg-[#5B99C2] text-white px-6 py-2.5 font-semibold hover:shadow-lg hover:drop-shadow transition duration-200">
-                <Link href="/login">Login</Link>
-              </button>
-            </div>
-
-            <div className="flex items-center justify-center lg:hidden">
-              <button
-                className="focus:outline-none text-slate-200 dark:text-white"
-                onClick={toggleMenu}
-              >
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 20 20"
-                  aria-hidden="true"
-                  className="text-2xl text-slate-800 dark:text-white focus:outline-none active:scale-110 active:text-red-500"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
+              <div className="flex items-center justify-center lg:hidden">
+                <button
+                  className="focus:outline-none text-slate-200 dark:text-white"
+                  onClick={toggleMenu}
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </button>
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 20 20"
+                    aria-hidden="true"
+                    className="text-2xl text-slate-800 dark:text-white focus:outline-none active:scale-110 active:text-red-500"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
             </div>
-          </div>
 
-          {isOpen && (
-            <div className="lg:hidden mt-2">
-              <ul className="flex flex-col space-y-2 text-base font-bold text-black/60 dark:text-white">
-                <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
-                  <Link href="/">Home</Link>
-                </li>
-                <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
-                  <Link href="/dashboard">Dashboard</Link>
-                </li>
-                <li className="pt-4">
-                  <button className="block w-full text-left rounded-md bg-[#F5EDED] text-black px-6 py-2.5 font-semibold hover:shadow-lg hover:drop-shadow transition duration-200">
-                    <Link href="/register">Sign up</Link>
-                  </button>
-                </li>
-                <li>
-                  <button className="block w-full text-left rounded-md bg-[#5B99C2] text-white px-6 py-2.5 font-semibold hover:shadow-lg hover:drop-shadow transition duration-200">
-                    <Link href="/login">Login</Link>
-                  </button>
-                </li>
-              </ul>
-            </div>
-          )}
-        </nav>
+            {isOpen && (
+              <div className="lg:hidden mt-2">
+                <ul className="flex flex-col space-y-2 text-base font-bold text-black/60 dark:text-white">
+                  <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
+                    <Link href="/">Home</Link>
+                  </li>
+                  <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
+                    <Link href="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <button className="block w-full text-left rounded-md bg-[#5B99C2] text-white px-6 py-2.5 font-semibold hover:shadow-lg hover:drop-shadow transition duration-200">
+                      <Link href="/login">Login</Link>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </nav>
+        </div>
       </div>
     </>
   );
